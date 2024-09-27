86f6bc09912c20f95ee860e2ff3522206c2d1e9a
Eradax <erikadebahr@gmail.com>
2024-09-27 17:46:23 +0200

    Added solutions to Udda tal

---
+++ cpp/uddatal.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define rep(i, high) for (int i = 0; i < high; i++)
+#define repp(i, low, high) for (int i = low; i < high; i++)
+#define sz(container) ((int)container.size())
+#define all(x) begin(x),end(x)
+
+void fast() { ios::sync_with_stdio(0); cin.tie(0); }
+
+
+int main() {
+    fast();
+
+    int n;
+
+    cin >> n;
+
+    if (n % 2 == 0) {
+        cout << "nej\n";
+    } else {
+        cout << "ja\n";
+    }
+
+    return 0;
+}
