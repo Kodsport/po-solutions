39a77c77a625483f2d7ca53fa27edca27f7112de
Matistjati <matistjati@outlook.com>
2024-10-02 03:22:35 +0200

    [apples] clearer

--- python/apples.py
+++ python/apples.py
@@ -1,27 +1,21 @@
-def count(x, y, visited_count):
-    global visited
-
-    if x < 0 or n <= x:
-        return 0
-    if visited[y][x]:
+# Insikt: vi vill aldrig gå åt vänster, bara upp/ner/höger
+def count(r, c, visited_row, visited_count):
+    if r < 0 or r >= 2 or c >= n:
         return 0
     if visited_count == k:
         return 0
 
-    visited[y][x] = True
-    apples = grid[y][x] + max(count(x+1, y, visited_count+1),
-                              count(x-1, y, visited_count+1),
-                              count(x, 1-y, visited_count+1))
-    visited[y][x] = False
-
+    # Vi kan alltid gå åt höger
+    best_from_here = count(r, c+1, 0, visited_count+1)
+    # Vi kan inte gå upp, ner och sen upp igen. För att förbjuda
+    # detta håller vi koll på om vi besökt den andra på den här raden
+    if visited_row == 0:
+        best_from_here = max(best_from_here, count(1-r, c, 1, visited_count+1))
+    apples = grid[r][c] + best_from_here
     return apples
-
 n, k = map(int, input().split())
 north = list(map(int, input().split()))
 south = list(map(int, input().split()))
-
 grid = [north, south]
-visited = [[False for _ in range(n)],
-           [False for _ in range(n)]]
 
-print(count(0, 1, 0))
+print(count(1, 0, 0, 0))
